f1e57c6ac9ce2ff1c7b3cd5469ee249a95077a9f
Avner Cohen <israbirding@gmail.com>
2014-12-22 09:06:08 +0200

    Initial Commit

---
+++ 5b.js
@@ -0,0 +1,118 @@
+/* Commands
+ ==> 5b echo 123
+ ==> 5b ping
+ .. Now make your own.
+ */
+
+var request = require('request'),
+  botName = process.env.BOT_NAME || '5b',
+  hipToken = process.env.HIP_TOKEN,
+  hipRoomName = process.env.HIP_ROOM,
+  postUrl = 'https://api.hipchat.com/v2/room/@PLACE_HOLDER@/notification?auth_token=' + hipToken,
+  readMessageUrl = 'https://api.hipchat.com/v2/room/@PLACE_HOLDER@/history/latest?auth_token=' + hipToken,
+  headers = {
+    'Content-Type': 'application/json'
+  };
+
+
+var requestPayload = {
+  "message": "",
+  "notify": true,
+  "color": "purple",
+  "message_format": "html"
+}
+
+var lastMessages = {};
+var MAX_MSG_LENGTH = 100;
+
+function postMessageToRoom(room, message, callBack) {
+  requestPayload['message'] = message;
+  var targetUrl = postUrl.replace('@PLACE_HOLDER@', room);
+
+  request.post({
+    headers: headers,
+    url: targetUrl,
+    json: requestPayload
+  }, function(error, response, body) {
+    callBack(error);
+  });
+};
+
+function listenForRoom() {
+  var room = hipRoomName;
+
+  var targetUrl = readMessageUrl.replace('@PLACE_HOLDER@', room);
+
+  request.get({
+    headers: headers,
+    url: targetUrl
+  }, function(error, response, body) {
+    var results = JSON.parse(body);
+    if(!results.items) {
+      return;
+    }
+
+    if(Object.keys(lastMessages).length === 0) {
+      console.log(botName + ' is alive!');
+      results.items.forEach(function(item) {
+        lastMessages[item.id] = true
+      });
+    } else {
+      results.items.forEach(function(item) {
+        var newMessage = typeof(lastMessages[item.id]) === 'undefined';
+        lastMessages[item.id] = true;
+        var instruction = item.message;
+        if(newMessage && instruction.indexOf(botName) === 0) {
+          performAction(instruction);
+        }
+      });
+    }
+    cleanUpIfNeeded();
+  });
+
+}
+
+
+//Since we use this ugly LIFO queue for new message.. we clean this here to limit hash size.
+function cleanUpIfNeeded() {
+  if(Object.keys(lastMessages).length > MAX_MSG_LENGTH) {
+    var toBeDelete = [];
+    //perform cleanup
+    var counter = Object.keys(lastMessages).length - MAX_MSG_LENGTH;
+    Object.keys(lastMessages).forEach(function(key, value) {
+      counter--;
+      if(counter < 0) {
+        return;
+      }
+      toBeDelete.push(key);
+    });
+    toBeDelete.forEach(function(key) {
+      lastMessages[key] = null;
+      delete lastMessages[key];
+    });
+  }
+}
+
+function performAction(instruction) {
+  console.log('Request  - /' + instruction + '/')
+  var moduleToExecute = instruction.replace(botName, '').trim();
+  var params = moduleToExecute.split(' ');
+  var plugin = null;
+  try {
+    plugin = require('./plugins/' + params[0])
+  } catch(e) {
+    console.error(e);
+    postMessageToRoom(hipRoomName, 'b5 sais: \'Damn! failed executing: /' + params[0] + '/', errorOnError);
+    return false;
+  }
+  var results = plugin.run(params);
+  postMessageToRoom(hipRoomName, results, errorOnError);
+}
+
+function errorOnError(err) {
+  if(err) {
+    console.err(err)
+  }
+}
+
+setInterval(listenForRoom, 1500);
---
+++ readme.md
@@ -0,0 +1,36 @@
+<pre>
+ ______    _______      
+/_____/\ /_______/\     
+\::::_\/_\::: _  \ \    
+ \:\/___/\\::(_)  \/_   
+  \_::._\:\\::  _  \ \  
+   /_____\/ \::(_)  \ \ 
+   \_____/   \_______\/ 
+                        
+</pre>
+
+5b Hipchat bot
+===
+
+(Background: As I was trying to setup a super simple hipchat bot, I just couldn't setup the many available ones, XMPP dependencies couldnot compile, missing libs, or very old code base.
+Since I wanted something that is so f'ing simple, I opted to set up me own version.
+)
+
+![Example using 5b](./5b_sample.gif)
+
+
+### Running the Bot
+
+(Note that ES6 Maps are used, so, if you opt for manual start, add the --harmony flag to node)
+<pre>
+$> npm install
+$> HIP_TOKEN=your_hipchat_api_token HIP_ROOM=room_name_or_id npm start
+$> Boom!
+</pre>
+
+#### "Architecture"
+(Big word for small code base)
+I want it to be simple, and extendible. I am perfectly fine with getting the bot to response in a delay of 2-5 seconds.
+So:
+1. Plugins folder is where you set up your methods. Each module should export method "run()", that's pefroms the actual command and reply with a string of some sort.
+2. Every ~1.5sec a new message is searched for.
